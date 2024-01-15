cad087e652f10e4204cb6cebcb849ecab916b976
imjayapple <imjayapple@gmail.com>
2024-01-14 19:04:13 -0800

    Different style of image carousel is needed

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-let currentIndex = 0;
+let slideIndex = 1;
+showSlides(slideIndex);
 
-function nextSlide() {
-    currentIndex = (currentIndex + 1) % 3; // 3 is the number of slides //
-    updateSlider();
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function updateSlider() {
-    const slider = document.querySelector('.image-slider');
-    slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
 
-// Cycle slide every 3 seconds
-setInterval(nextSlide, 3000);
-
-// Initial update
-updateSlider();
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
